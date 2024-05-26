2c338e3af21cf0a947cf8a16c41a2cfa443361ce
El-hacen21 <diallosegue@gmail.com>
2024-05-26 21:57:30 +0000

    Add clear file input filed in Mint

--- src/components/Mint/Mint.tsx
+++ src/components/Mint/Mint.tsx
@@ -1,21 +1,19 @@
-
-import './Mint.css'
-import React, { useState, useEffect } from 'react';
+import './Mint.css';
+import React, { useState, useEffect, useRef } from 'react';
 import uploadIcon from "../../assets/img/upload-icon.jpg";
 import { useFhevm } from '../Contexts/useFhevm';
-import { mintToken, getAccount } from '../Blockchain/contract'
-import { exportCryptoKey, generateKey } from '../Utils/keyencrypt'
+import { mintToken, getAccount } from '../Blockchain/contract';
+import { exportCryptoKey, generateKey } from '../Utils/keyencrypt';
 import { NFTContent } from '../Contexts/NFTContext';
 import { useNFTs } from '../Contexts/useNFTs';
-import { encryptFile, uploadFileToIPFS } from '../Utils/utils'
-import { toast } from 'react-toastify'
-
+import { encryptFile, uploadFileToIPFS } from '../Utils/utils';
+import { toast } from 'react-toastify';
 
 export const Mint = () => {
-
-  const [fileInfo] = useState({ name: '', status: '' });
+  const [fileInfo, setFileInfo] = useState({ name: '', status: '' });
   const { instance, createInstance } = useFhevm();
   const { addNFT } = useNFTs();
+  const fileInputRef = useRef<HTMLInputElement>(null); // Create a ref for the file input
 
   const showMintingErrorToast = () => {
     const content = (
@@ -36,6 +34,7 @@ export const Mint = () => {
     if (!e.target.files || e.target.files.length === 0) return;
 
     const file = e.target.files[0];
+    setFileInfo({ name: file.name, status: 'Minting...' });
 
     try {
       const fileKey = await generateKey();
@@ -43,9 +42,10 @@ export const Mint = () => {
       const account = await getAccount();
       if (!account) {
         toast.info('Merci de vous connecter pour continuer.');
+        setFileInfo({ name: '', status: '' });
         return;
       }
-      if (!instance) throw new Error("Intance retrieval failed.");
+      if (!instance) throw new Error("Instance retrieval failed.");
 
       const ciphFile = await encryptFile(file, fileKey);
 
@@ -53,7 +53,7 @@ export const Mint = () => {
 
       const cidHash = await uploadFileToIPFS(ciphFile);
 
-      toast.info("Your file is currently being minted as an NFT. This may take a few moments.");
+      // toast.info("Your file is currently being minted as an NFT. This may take a few moments.");
 
       const token = await mintToken(cidHash, encryptedFileKey);
 
@@ -61,36 +61,39 @@ export const Mint = () => {
         const nftContent: NFTContent = { id: Number(token.tokenId), file: file };
         addNFT(nftContent);
         toast.success("The File has been minted as an NFT and will soon appear in your gallery!");
+        setFileInfo({ name: file.name, status: 'Minted successfully' });
       }
 
     } catch (error) {
       console.log("Failed to mint:", error);
       showMintingErrorToast();
+      setFileInfo({ name: file.name, status: 'Minting failed' });
     }
+
+    e.target.value = ''; // Clear the file input field
+
+    fileInputRef.current?.blur(); // Blur the file input field to remove focus
   };
 
   const fileKeyEncryption = async (fileKey: CryptoKey): Promise<Uint8Array[]> => {
-    if (!instance) throw new Error("Intance retrieval failed.");
+    if (!instance) throw new Error("Instance retrieval failed.");
     const encryptedFileKey: Uint8Array[] = [];
 
     const keySegments = await exportCryptoKey(fileKey);
     for (const segment of keySegments) {
       const encrypted = instance.encrypt64(segment);
-      console.log("ecrypted64 : ", encrypted);
+      console.log("encrypted64 : ", encrypted);
       encryptedFileKey.push(encrypted);
     }
 
     return encryptedFileKey;
   };
 
-
-
   useEffect(() => {
     if (!instance) {
       createInstance().catch(console.error);
     }
   }, [instance, createInstance]);
-
 
   return (
     <section className="mint" id="mint">
@@ -99,11 +102,11 @@ export const Mint = () => {
           <div className="col-12">
             <div className="mint-bx wow zoomIn">
               <h2>Convert Files to NFTs</h2>
-              {/* <p>Drag and drop your files here or click to select files to upload.</p> */}
               <div className="file-upload-container">
                 <label htmlFor="file-upload-input" className="file-upload-label">
                   <input
                     id="file-upload-input"
+                    ref={fileInputRef} // Attach the ref to the file input
                     type="file"
                     style={{ display: 'none' }}
                     onChange={EncryptThenMint}
@@ -115,15 +118,16 @@ export const Mint = () => {
                   </div>
                   <div className="file-types-explanation">.jpg, .jpeg, .png, .gif, .pdf, .doc, .docx, .mp4</div>
                 </label>
-                <p className="upload-explanation">The uploaded files are securely and confidentially saved on IPFS and accessible only by you or the people you have shared it with using Zama's fhEVM.</p>
+                <p className="upload-explanation">
+                  The uploaded files are securely and confidentially saved on IPFS and accessible only by you or the people you have shared it with using Zama's fhEVM.
+                </p>
                 {fileInfo.status && <p className="file-upload-status">{fileInfo.status}</p>}
                 {fileInfo.name && <p className="uploaded-file-info">Name: {fileInfo.name}</p>}
               </div>
-
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
